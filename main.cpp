d55fd4deb47e4045f92d3ac9667327303f1d9b7a
Mental Blok <45437099+MentalBlok@users.noreply.github.com>
2025-03-19 15:52:56 -0400

    Update main.cpp
    Use better option processing

--- main.cpp
+++ main.cpp
@@ -4,42 +4,50 @@
 
 #include <iomanip>
 #include <iostream>
-#include <time.h>
-#include <unistd.h>
+#include <getopt.h>
 #include "Mhash.h"
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
-  int gen = 0;
-  int nIter = 0;
-  char ch = 0;
-  while ((ch = getopt(argc, argv, "g t: ? ")) != EOF) {
-    switch (ch) {
-    case '?':
-      printf("-g  Generate the switch statement that does the hash\n");
-      printf("-t: Test the hash function in a tight loop N times.\n");
-      exit(0);
-    case 'g':
-      gen++;
-      break;
-    case 't':
-      sscanf(optarg, "%d", &nIter);
-      break;
-    default:
-      break;
+void printUsage() {
+    cout << "-g  Generate the switch statement that does the hash" << endl;
+    cout << "-t  Test the hash function in a tight loop N times." << endl;
+}
+
+int main(int argc, char **argv) {
+    bool generateSwitchStatement = false;
+    int numIterations = 0;
+    int option = 0;
+
+    while ((option = getopt(argc, argv, "gt:")) != -1) {
+        switch (option) {
+            case 'g':
+                generateSwitchStatement = true;
+                break;
+            case 't':
+                try {
+                    numIterations = stoi(optarg);
+                } catch (const invalid_argument& e) {
+                    cerr << "Invalid number of iterations: " << optarg << endl;
+                    return 1;
+                }
+                break;
+            case '?':
+            default:
+                printUsage();
+                return 1;
+        }
     }
-  }
-  Mhash mhash;
-  if (gen) {
-    mhash.generate();
-    exit(0);
-  }
-  if (nIter) {
-    mhash.timeHash(nIter);
-    exit(0);
-  }
-  mhash.testHash();
-  return 0;
+
+    Mhash mhash;
+    if (generateSwitchStatement) {
+        mhash.generate();
+        return 0;
+    }
+    if (numIterations > 0) {
+        mhash.timeHash(numIterations);
+        return 0;
+    }
+    mhash.testHash();
+    return 0;
 }
